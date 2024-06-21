4a87703ac3c5b76de5f3cfd67d8f2db78d9059f8
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-06-21 10:47:37 +0530

    Added Inner Classd package

---
+++ Practice/InnerClass/IC.java
@@ -0,0 +1,25 @@
+package InnerClass;
+
+
+class A{
+    int age;
+
+    public void show(){
+        System.out.println("in show");
+    }
+
+    class B{
+        public void config(){
+            System.out.println("in config");
+        }
+    }
+}
+public class IC {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        A.B obj1 = obj.new B();
+        obj1.config();
+    }
+}

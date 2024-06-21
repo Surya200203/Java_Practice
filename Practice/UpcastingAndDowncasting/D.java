1f264d7f746d8919eb86de224a64f84740392357
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-06-21 10:50:44 +0530

    Added Upcasting and Doencasting package

---
+++ Practice/UpcastingAndDowncasting/D.java
@@ -0,0 +1,24 @@
+package UpcastingAndDowncasting;
+
+class A{
+    public void show1(){
+        System.out.println("in A show");
+    }
+}
+
+class B extends A {
+    public void show2(){
+        System.out.println("in B show");
+    }
+}
+
+
+public class D {
+    public static void main(String[] args) {
+        A obj = new B();        // Upcasting
+        obj.show1();
+
+        B obj1 = (B) obj;
+        obj1.show2();
+    }
+}
